245d68598987486c82f379cf387eb3892176d7aa
Deanamic <deanzhu1@gmail.com>
2017-01-29 22:19:31 +0100

    updated segment tree

---
+++ Data-structures/Segment_Tree.cc
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Segment Tree/ Interval Tree
+ * Stores information in a Heap
+ * Modification, lookup in O(logn)
+ * All intervals are [ , )
+*/
+
+struct SegmentTree{
+    vector<int> Seg;
+	int n;
+
+	void build(vector<int>& v, int m){
+		Seg = vector<int> (4*n);
+		n = m;
+		build(v, 1, 0, n);
+	}
+	void build( vector<int>& v, int id,int left, int right){
+		if(left + 1 >= right){
+			Seg[id] = v[left];
+			return;
+		}
+		int mid = (left + right)/2;
+		build(v, id * 2, left, mid);
+		build(v, id * 2 + 1, mid, right);
+		Seg[id] = Seg[id*2] + Seg[id*2 + 1];
+	}
+
+	int find(int x, int y){
+		return find(x, y, 1, 0, n);
+	}
+	int find(int x, int y, int id, int left, int right){
+		if(left >= y or right <= x){
+			return 0;
+		}
+		if(left >= x and right <= y){
+			return Seg[id];
+		}
+		int mid = (left + right)/2;
+		return find(x, y, 2*id, left, mid) + find(x, y, 2*id + 1, mid, right);
+	}
+
+	int update(int p, int new_value){
+		update(p, new_value, 1, 0, n);
+	}
+	void update(int p, int new_value, int id, int left, int right){
+		if(left + 1 >= right){
+			Seg[id] = new_value;
+			return;
+		}
+		int mid = (left+right)/2;
+		if(p < mid) update(p, new_value, 2*id, left, mid);
+		else update(p, new_value, 2*id + 1, mid, right);
+		Seg[id] = Seg[id*2] + Seg[id*2 + 1];
+	}
+};
