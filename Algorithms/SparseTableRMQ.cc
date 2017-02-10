dbb4c5a92d46924a9a10c7802a751dfbf2f96c4b
Deanamic <deanzhu1@gmail.com>
2017-02-10 22:17:12 +0100

    modified sparsetable, now ST stores index in lieu of value

--- Algorithms/SparseTableRMQ.cc
+++ Algorithms/SparseTableRMQ.cc
@@ -12,23 +12,31 @@ using namespace std;
 struct SparseTable {
     vector<vector<int> > ST;
     vector<int > P;
+    vector<int> v;
     int N;
     int MAXLOG = 0;
-    void build(int n, const vector<int>& v){
+    void build(int n){
         N = n;
         while ((1 << MAXLOG) <= N) ++MAXLOG;
         ST = vector<vector<int> > (N, vector<int> (MAXLOG));
         P = vector<int> (N+1);
+        v = vector<int> (N);
+        for (int i = 0; i < N; ++i) {
+            cin >> v[i];
+        }
 
         int LOG = 0;
         for(int i = 1; i < N + 1; ++i){
             P[i] = ((1<<LOG) > i ?  LOG-1 : ++LOG-1);
         }
         // Dynamic Sparse table building
-        for (int j = 0; j < MAXLOG; ++j) {
-            for (int i = 0; i < N; ++i) {
-                if (i + (1 << j) - 1 < N)
-                    ST[i][j] = (j ? min(ST[i][j - 1], ST[i + (1 << (j - 1))][j - 1]) : v[i]);
+        for(int i = 0; i < N; ++i) ST[i][0] = i;
+        for (int j = 1; j < MAXLOG; ++j) {
+            for (int i = 0; i + (1 << j) - 1 < N; ++i) {
+                if (v[ST[i][j - 1]] < v[ST[i + (1 << (j - 1))][j - 1]])
+                     ST[i][j] = ST[i][j - 1];
+                else
+                     ST[i][j] = ST[i + (1 << (j - 1))][j - 1];
             }
         }
         // ST[i][j] is the minimum of a interval of length 2^j
@@ -47,21 +55,16 @@ struct SparseTable {
         // hence we need the maximum LOG, such that 2^LOG is smaller than
         // the range or [l,r]
         // Which has been preprocessed before
-        return min(ST[l][LOG], ST[r - (1 << LOG) + 1][LOG]);
+        return min(v[ST[l][LOG]], v[ST[r - (1 << LOG) + 1][LOG]]);
 
     }
 };
 
 int main(){
     int n;
-
     cin >> n;
-    vector<int> v(n);
-    for (int i = 0; i < n; ++i) {
-        cin >> v[i];
-    }
     SparseTable S;
-    S.build(n, v);
+    S.build(n);
     int m;
     cin >> m;
     for (int i = 0; i < m; ++i) {
