e18425489a047a2ae69adbc2569f2a2672c6ef14
Dean Zhu <deanzhu1@gmail.com>
2017-04-19 00:08:03 +0200

    fixed a bug in lazy

--- Data-structures/SegmentTreeLazy.cc
+++ Data-structures/SegmentTreeLazy.cc
@@ -6,16 +6,16 @@ using namespace std;
  * Modification, lookup in O(logn)
  * Provides Tools to update intervals
  * All intervals are [ , )
-*/
+ */
 
 struct LazySegmentTree{
-    vector<int> Seg;
-    vector<int> lazy;
-    int n;
+	vector<int> Seg;
+	vector<int> lazy;
+	int n;
 
-    void build(vector<int>& v, int m){
+	void build(vector<int>& v, int m){
 		n = m;
-        Seg = lazy = vector<int> (4*n,0);
+		Seg = lazy = vector<int> (4*n,0);
 		build(v, 1, 0, n);
 	}
 	void build( vector<int>& v, int id,int left, int right){
@@ -29,18 +29,18 @@ struct LazySegmentTree{
 		Seg[id] = Seg[id*2] + Seg[id*2 + 1];
 	}
 
-    int find(int x, int y){
+	int find(int x, int y){
 		return find(x, y, 1, 0, n);
 	}
 	int find(int x, int y, int id, int left, int right){
-        if (lazy[id] != 0) {
-            Seg[id] +=  (right-left)*lazy[id];
-            if (x + 1 < y) {
-                lazy[2*id] += lazy[id];             // Lazy propagation
-                lazy[2*id + 1] += lazy[id];
-            }
-            lazy[id] = 0;
-        }
+		if (lazy[id] != 0) {
+			Seg[id] +=  (right-left)*lazy[id];
+			if (left + 1 < right) {
+				lazy[2*id] += lazy[id];             // Lazy propagation
+				lazy[2*id + 1] += lazy[id];
+			}
+			lazy[id] = 0;
+		}
 		if(left >= y or right <= x){
 			return 0;
 		}
@@ -51,34 +51,34 @@ struct LazySegmentTree{
 		return find(x, y, 2*id, left, mid) + find(x, y, 2*id + 1, mid, right);
 	}
 
-    int updateinterval(int p, int x, int y){
+	int updateinterval(int p, int x, int y){
 		updateinterval(p, 1, x, y, 0, n);
 	}
 
-    void updateinterval (int p, int id, int x, int y, int left, int right) {
-        if (lazy[id] != 0) {
-            Seg[id] +=  (right-left)*lazy[id];
-            if (x + 1 < y) {
-                lazy[2*id] += lazy[id];
-                lazy[2*id + 1] += lazy[id];
-            }
-            lazy[id] = 0;
-        }
-        if(left >= y or right <= x){
+	void updateinterval (int p, int id, int x, int y, int left, int right) {
+		if (lazy[id] != 0) {
+			Seg[id] +=  (right-left)*lazy[id];
+			if (x + 1 < y) {
+				lazy[2*id] += lazy[id];
+				lazy[2*id + 1] += lazy[id];
+			}
+			lazy[id] = 0;
+		}
+		if(left >= y or right <= x){
 			return;
 		}
 		if(left >= x and right <= y){
-            Seg[id] +=  (right-left)*p;
-            if (x + 1 < y)  {
-                lazy[2*id] += p;
-                lazy[2*id + 1] += p;
-            }
-            return;
-        }
-        int mid = (left + right)/2;
-        updateinterval (p, 2*id, x, y, left, mid);
-        updateinterval (p, 2*id + 1, x, y, mid, right);
-        Seg[id] = Seg[2*id] + Seg[2*id + 1];
+			Seg[id] +=  (right-left)*p;
+			if (left + 1 < right)  {
+				lazy[2*id] += p;
+				lazy[2*id + 1] += p;
+			}
+			return;
+		}
+		int mid = (left + right)/2;
+		updateinterval (p, 2*id, x, y, left, mid);
+		updateinterval (p, 2*id + 1, x, y, mid, right);
+		Seg[id] = Seg[2*id] + Seg[2*id + 1];
 
-    }
+	}
 };
