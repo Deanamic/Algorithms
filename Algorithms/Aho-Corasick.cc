8a7bba5a4f79c7f7a6a6957eb3a9abfccfe46f85
Dean Zhu <deanzhu1@gmail.com>
2017-04-22 13:32:30 +0200

    added Aho corasick

--- Algorithms/Aho-Corasick.cc
+++ Algorithms/Aho-Corasick.cc
@@ -45,7 +45,7 @@ int go(int p, char c){
 
 int suf(int p){
     if(sufl[p] != -1) return sufl[p];
-    if(p == 0) return sufl[p] = -1;
+    if(p == 0) return sufl[p] = 0;
     if(TRIE[p].pare == 0) return sufl[p] = 0;
     return sufl[p] = go(suf(TRIE[p].pare),TRIE[p].val);
 }
@@ -54,7 +54,7 @@ bool dfs_cycle(int x, int p){
     if(v[x] == 2) return true;
     v[x] = 2;
     for(int i = 0; i < 2; ++i){
-        if(v[g[i][x]] != 1 and g[i][x] != p){
+        if(v[g[i][x]] != 1 and !TRIE[g[i][x]].end){
             if (dfs_cycle(g[i][x],x)) return true;
         }
     }
@@ -62,7 +62,7 @@ bool dfs_cycle(int x, int p){
     return false;
 }
 
-void addTRIE(string s){
+void addTRIE(string& s){
     int pos = 0;
     for(int i = 0; i < s.size(); ++i){
         if(s[i] == '0'){
@@ -94,7 +94,6 @@ int main(){
     int n;
     cin >> n;
     TRIE.push_back({'&',0,0,0,0});
-
     while(n--){
         string s;
         cin >> s;
@@ -104,29 +103,20 @@ int main(){
     g.resize(2,vector<int> (CUR,-1));
     queue<int> q;
     q.push(0);
-    vector<int > can(CUR,0);
     vector<int> vis(CUR,-1);
     v.resize(CUR,0);
     while(q.size()){
         int p = q.front(); q.pop();
-        if(!TRIE[p].end){
-            if(vis[p] == -1){
-                vis[p] = 1;
-                q.push(go(p,'0'));
-                q.push(go(p,'1'));
-                
-            }
-        }
-        else{
-            v[p] = 1;
-        }
-    }
-    /*
-    for(int i = 0; i < 2; ++i){
-        for(int j = 0; j < CUR; ++j){
-            cerr << g[i][j] << ' ';
-        }cerr << endl;
-    }*/
+	TRIE[p].end |= TRIE[suf(p)].end;
+	if(!TRIE[p].end){
+	    if(vis[p] == -1){
+		vis[p] = 1;
+		q.push(go(p,'0'));
+		q.push(go(p,'1'));
+	    }
+	}
+	else v[p] = 1;
+    } 
     if(dfs_cycle(0,-1)) cout << "YES" << endl;
     else cout << "NO" << endl;
 }
