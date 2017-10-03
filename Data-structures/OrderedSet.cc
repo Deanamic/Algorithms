85f324f9f9437ea946ca2773468fa9fb2b28ae45
Dean Zhu <deanzhu1@gmail.com>
2017-10-03 14:31:05 +0200

    Renamed Files

---
+++ Data-structures/OrderedSet.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+using namespace std;
+using namespace __gnu_pbds;
+
+template<typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+#define POS(X,Y) *X.find_by_order(Y)
+#define ORD(X,Y) X.order_of_key(Y)
+/*
+ * tree actua como un set pero con dos funciones extra
+ * find_by_order() que devuelve un iterador al elemento k-esimo
+ *
+ * order_of_key() que devuelve el numero de elementos mas pequeños
+ * estrictos que X
+ *
+ * Complejidad:
+ * 	-insert 				O(logn)
+ * 	-erase 					O(logn)
+ * 	-findbyorder 			O(logn)
+ * 	-orderofkey	 			O(logn)
+ * 	-iterate thourgh set 	O(n)
+*/
