57009d3ca1e7cf18724af9535c740466ef3997f6
Dean <deanzhu1@gmail.com>
2017-10-26 14:50:50 +0200

    Update Eulerian_cycle.cc

--- Problems/Eulerian_cycle.cc
+++ Problems/Eulerian_cycle.cc
@@ -1,19 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/*
-Given a connected undirected graph
-There exists an Eulerian cycle iff all vertices have an even degree
-There exists an Eulerian tour iff all but 2 vertices have an even degree
-
-Given a connected directed Graph
-There exists an Eulerian cycle iff all vertices have the same number of entering and exiting edges
-There exists an Eulerian tour iff all vertices but 2 have the same entering and exiting edges and the other 2 have a |difference| of 1.
-
-*/
-
 //undirected graph
-
 struct edge{
     int u, v;
     bool used;
@@ -25,6 +13,7 @@ vector<int> nxt;
 vector<int> cycle;
 int p;
 
+//returns de eulerian cycle starting at u, cycle is in reverse order
 void find_cycle(int u){
     while(nxt[u] < adj[u].size()){
         int go = adj[u][nxt[u]++];
@@ -34,19 +23,19 @@ void find_cycle(int u){
             find_cycle(to);
         }
     }
-    cycle[--p] = u;
+    cycle.push_back(u);
 }
 
 //directed graph
 vector<int> next; //stores last visited vertex index
-vector<vector<int>> adj(n);
-vector<int> cycle(m);
-int p = m; //m is the number of edges 
+vector<vector<int>> adj;
+vector<int> cycle;
 
+//returns the eulerian cycle with reverse order
 void find_cycle(int u){
-    while(next[u] <= adj[u].size())
+    while(next[u] < adj[u].size())
         find_cycle(adj[u][next[u]++]);
-    cycle[p--] = u;
+    cycle.push_back(u);
 }
 
 int main(){
